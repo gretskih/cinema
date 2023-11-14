8541f0e210b9564950037a81b8de475ae7c3bef7
AGretskikhHome <gretskih@mail.ru>
2023-11-14 22:23:38 +0300

    Написан README.md

--- src/main/java/ru/job4j/cinema/service/FilmSessionService.java
+++ src/main/java/ru/job4j/cinema/service/FilmSessionService.java
@@ -5,9 +5,10 @@ import ru.job4j.cinema.preview.FilmPreview;
 import ru.job4j.cinema.preview.SessionPreview;
 
 import java.util.Collection;
+import java.util.Map;
 
 public interface FilmSessionService {
     FilmSession findById(int id);
 
-    Collection<SessionPreview> findAll();
+    Map<String, Collection<SessionPreview>> findAll();
 }
